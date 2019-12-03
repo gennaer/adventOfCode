5d5275fbf352fe4f170a961d431f51a61b922774
gennaer <4304873+gennaer@users.noreply.github.com>
2019-12-02 20:40:54 -0800

    finish p2 prob2

--- 2019/prob2/prob2.py
+++ 2019/prob2/prob2.py
@@ -3,14 +3,14 @@ codes = []
 with open('input.txt','r') as f:
 	d = f.readline().split(',') # assuming just one line
 	for opcode in d:
-		# for alternate way of doing this below
 		codes.append(int(opcode)) 
 
 # make adjustments as required by problem:
 codes[1] = 12
 codes[2] = 2
+orig_code = codes.copy() # for alternate way of doing problem
 
-# alternate way by breaking apart into chunks of 4:
+# here's one way to do this: break into chunks of 4
 from itertools import zip_longest
 def grouper(iterable, n, fillvalue=None):
     "Collect data into fixed-length chunks or blocks"
@@ -29,3 +29,44 @@ for opc, c1, c2, p in grouper(codes, 4):
 		print('Uh Oh') # not doing a nice exception here because lazy
 
 print(codes[0])
+
+# ok after reading part 2, that's probably not what they intended
+# let's try a different way:
+def execute_code(intcode):
+	inst_ptr = 0
+	inst_end = False
+	while not inst_end:
+		opc = intcode[inst_ptr]
+		p1 = intcode[inst_ptr+1]
+		p2 = intcode[inst_ptr+2]
+		p3 = intcode[inst_ptr+3]
+		if opc == 99:
+			inst_end = True
+			continue
+		elif opc == 1:
+			intcode[p3] = intcode[p1]+intcode[p2]
+		elif opc == 2:
+			intcode[p3] = intcode[p1]*intcode[p2]
+		inst_ptr += 4
+	return intcode[0]
+
+codes2 = orig_code.copy()
+print(execute_code(codes2))
+
+
+
+# now let's try part 2:
+target = 19690720
+param_found = False
+for noun in range(100):
+	for verb in range(100):
+		test_code = orig_code.copy()
+		test_code[1] = noun
+		test_code[2] = verb
+		if execute_code(test_code) == target:
+			param_found = True
+			break
+	if param_found:
+		break
+
+print(100*noun+verb)
